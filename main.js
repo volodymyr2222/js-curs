5795eff7bb2088e22e5d2783294c2dd5b070c42d
volodymyr2222 <volody9089@gmail.com>
2021-06-03 20:49:05 +0300

    js-1

---
+++ main.js
@@ -0,0 +1,58 @@
+let a = false;
+console.log(typeof a);
+
+let b = "hello world"
+console.log(typeof b);
+
+let c = 22222;
+console.log(typeof c);
+
+let d = 2121121n;
+console.log(typeof d);
+
+let e = Symbol();
+console.log(typeof e);
+
+let f = document.querySelector('info');
+console.log(f);
+
+let g ;
+console.log(typeof g);
+
+let obj = {
+    name: 'miha'
+} ;
+console.log(typeof obj);
+
+
+let aa = "22";
+aa = Number(aa);
+console.log(typeof aa);
+
+let bb = 22;
+bb = String(bb);
+console.log(typeof bb);
+
+let cc = true;
+cc = String(cc);
+console.log(typeof cc);
+
+let dd = "true";
+dd = Boolean(dd);
+console.log(typeof dd);
+
+
+
+let info = +prompt("");
+let info2 = +prompt("");
+alert(info + info2);
+
+
+let info3 = prompt('');
+let info4 = 5;
+alert(info3 / info4);
+
+let info5 = confirm('klic');
+console.log(info5);
+
+
