77a2aa07a9c84856dc832fa6f47c62e63f751dfa
volodymyr2222 <volody9089@gmail.com>
2021-06-13 16:00:06 +0300

    task6

--- dz-1/main.js
+++ dz-1/main.js
@@ -1,57 +1,59 @@
-let a = false;
-console.log(typeof a);
+// let a = false;
+// console.log(typeof a);
 
-let b = "hello world"
-console.log(typeof b);
+// let b = "hello world"
+// console.log(typeof b);
 
-let c = 22222;
-console.log(typeof c);
+// let c = 22222;
+// console.log(typeof c);
 
-let d = 2121121n;
-console.log(typeof d);
+// let d = 2121121n;
+// console.log(typeof d);
 
-let e = Symbol();
-console.log(typeof e);
+// let e = Symbol();
+// console.log(typeof e);
 
-let f = document.querySelector('info');
-console.log(f);
+// let f = document.querySelector('info');
+// console.log(f);
 
-let g ;
-console.log(typeof g);
+// let g ;
+// console.log(typeof g);
 
-let obj = {
-    name: 'miha'
-} ;
-console.log(typeof obj);
-
-
-let aa = "22";
-aa = Number(aa);
-console.log(typeof aa);
-
-let bb = 22;
-bb = String(bb);
-console.log(typeof bb);
-
-let cc = true;
-cc = String(cc);
-console.log(typeof cc);
-
-let dd = "true";
-dd = Boolean(dd);
-console.log(typeof dd);
+// let obj = {
+//     name: 'miha'
+// } ;
+// console.log(typeof obj);
 
 
+// let aa = "22";
+// aa = Number(aa);
+// console.log(typeof aa);
 
-let info = +prompt("");
-let info2 = +prompt("");
-alert(info + info2);
+// let bb = 22;
+// bb = String(bb);
+// console.log(typeof bb);
+
+// let cc = true;
+// cc = String(cc);
+// console.log(typeof cc);
+
+// let dd = "true";
+// dd = Boolean(dd);
+// console.log(typeof dd);
 
 
-let info3 = prompt('');
-alert(info3 / 5);
 
-let info5 = confirm('klic');
-console.log(info5);
+// let info = +prompt("");
+// let info2 = +prompt("");
+// alert(info + info2);
+
+
+// let info3 = prompt('');
+// alert(info3 / 5);
+
+// let info5 = confirm('klic');
+// console.log(info5);
+
+
 
 
---
+++ dz-6/main.js
@@ -0,0 +1,35 @@
+// Task-1 Метод розгортання масива
+
+let arr = [1, 5, [333, 6], [8, [9]], 4, 101,];
+let result = arr.flat(Infinity);
+console.log(result);
+
+
+// Task-2 Метод сортування 
+
+let arrSort = [2,5,4,100,1,9,33,22]
+// сортування по зростанню чисел
+console.log(arrSort.sort(function(a,b){return a - b;}));
+
+// сортування по зростанню чисел
+console.log(arrSort.sort(function(a,b){ return b - a;})) 
+
+
+
+
+
+// Task-3 збільшити кожен масив на 5,10 і посумувати його
+
+let arrSum = [1,2,3,4,5,6];
+let arrSumOme = arrSum.reduce(function(previsionValue, item){
+    let a = item + 5;
+    let b = a +previsionValue;
+    return b
+},0);
+let arrSumTwo = arrSum.reduce(function(previsionValue, item){
+    let a = item + 10;
+    let b = a +previsionValue;
+    return b
+},0);
+
+console.log("Збільшення кожного масиву на П'ЯТЬ - ", arrSumOme, "  і на ДЕСЯТЬ - ", arrSumTwo);
